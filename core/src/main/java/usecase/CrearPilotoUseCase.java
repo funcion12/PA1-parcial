331c351d9b37ad38e6f5273a12541db9fe467f7b
Servilla <ezequielservilla33@gmail.com>
2025-05-27 17:38:41 -0300

    Subiendo archivos de Test

--- core/src/main/java/usecase/CrearPilotoUseCase.java
+++ core/src/main/java/usecase/CrearPilotoUseCase.java
@@ -1,15 +1,27 @@
 package usecase;
 
+import exception.ExceptionPilotoExiste;
 import input.ICrearPilotoInput;
 import model.Piloto;
 import output.ICrearPilotoOutput;
 
 public class CrearPilotoUseCase implements ICrearPilotoInput {
 
-    ICrearPilotoOutput crearPilotoOutput;
+    private final ICrearPilotoOutput crearPilotoOutput;
+
+    public CrearPilotoUseCase(ICrearPilotoOutput crearPilotoOutput) {
+        this.crearPilotoOutput = crearPilotoOutput;
+    }
 
     @Override
-    public boolean crearPiloto(Piloto piloto) {
+    public boolean crearPiloto(Piloto piloto) throws ExceptionPilotoExiste {
+        if(piloto == null) return false;
+
+        if(piloto.getNombre().isEmpty() || piloto.getDocumento().isEmpty() || piloto.getFecha_Nacimiento()==null || piloto.getId()==null || piloto.getNombre()==null) return false;
+
+        if(crearPilotoOutput.existe(piloto.getDocumento())) throw new ExceptionPilotoExiste("El piloto ya existe con ese documento");
+
         return crearPilotoOutput.guardarPiloto(piloto);
     }
+
 }
---
+++ core/src/test/java/usecase/CrearPilotoUseCaseTest.java
@@ -0,0 +1,18 @@
+package usecase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import output.ICrearPilotoOutput;
+
+@ExtendWith(MockitoExtension.class)
+public class CrearPilotoUseCaseTest {
+
+    @Mock
+    ICrearPilotoOutput crearPilotoOutput;
+
+    @Test
+    public void
+
+}
